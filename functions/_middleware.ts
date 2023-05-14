3efabff7be31b69ed6f776cccd731b757d5c260e
Hiromu OCHIAI <otiai10@gmail.com>
2023-05-14 11:42:50 +0900

    Add WWW-Authenticate

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -5,7 +5,10 @@ const BASICAUTH_PASS = "testtest";
 export const onRequest: PagesFunction = async ({ next, request }) => {
     const auth = request.headers.get("Authorization");
     if (!auth) {
-        return new Response("Authorization header not found", { status: 401 });
+        return new Response("Authorization header not found", {
+            status: 401,
+            headers: {"WWW-Authenticate": 'Basic realm="Hello"'},
+        })
     }
     const [schema, encoded] = auth.split(" ");
     if (!encoded || schema !== "Basic") {
