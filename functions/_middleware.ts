eb86cae58d61a9471d6006e98edfda4bc64729a2
Hiromu OCHIAI <otiai10@gmail.com>
2023-05-14 11:21:11 +0900

    Add small functions

---
+++ functions/_middleware.ts
@@ -0,0 +1,19 @@
+
+const BASICAUTH_USER = "test";
+const BASICAUTH_PASS = "testtest";
+
+export const onRequest: PagesFunction = async ({ next, request }) => {
+    const auth = request.headers.get("Authorization");
+    if (!auth) {
+        return new Response("Authorization header not found", { status: 401 });
+    }
+    const [schema, encoded] = auth.split(" ");
+    if (!encoded || schema !== "Basic") {
+        return new Response("Invalid authorization", { status: 401 });
+    }
+    const [user, pass] = atob(encoded).split(":");
+    if (user !== BASICAUTH_USER || pass !== BASICAUTH_PASS) {
+        return new Response("Invalid authorization", { status: 401 });
+    }
+    return await next();
+}
