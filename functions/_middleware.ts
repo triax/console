5ed04572cac51ac3361912c03ad26f54e646cf28
Hiromu OCHIAI <otiai10@gmail.com>
2023-05-20 16:48:27 +0900

    Fix interaction

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -3,20 +3,18 @@ const BASICAUTH_USER = "test";
 const BASICAUTH_PASS = "testtest";
 
 export const onRequest: PagesFunction = async ({ next, request }) => {
+    const onFailure = (msg) => new Response(msg, { status: 401, headers: { "WWW-Authenticate": 'Basic realm="Hello"' } });
     const auth = request.headers.get("Authorization");
     if (!auth) {
-        return new Response("Authorization header not found", {
-            status: 401,
-            headers: {"WWW-Authenticate": 'Basic realm="Hello"'},
-        })
+        return onFailure("Authorization header not found");
     }
     const [schema, encoded] = auth.split(" ");
     if (!encoded || schema !== "Basic") {
-        return new Response("Invalid authorization", { status: 401 });
+        return onFailure("Invalid authorization")
     }
     const [user, pass] = atob(encoded).split(":");
     if (user !== BASICAUTH_USER || pass !== BASICAUTH_PASS) {
-        return new Response("Invalid authorization", { status: 401 });
+        return onFailure("Invalid authorization");
     }
     return await next();
 }
